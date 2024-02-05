63d4af681968e4f729e5dc45a719680564a79048
Niraj Kc <nirajkc98@gmail.com>
2024-02-05 21:09:03 +0545

    cleanup backend

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,10 @@ const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-// /admin/add-product => GET
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
-// // /admin/products => GET
 router.get('/products', isAuth, adminController.getProducts);
 
-// /admin/add-product => POST
 router.post(
   '/add-product',
   [
